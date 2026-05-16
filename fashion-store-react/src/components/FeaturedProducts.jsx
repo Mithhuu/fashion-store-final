74eff14b3d9ad2f601881f7675fe25095b58e8c4
Mithhuu <mithuns2700@gmail.com>
2026-05-16 20:21:02 +0530

    Updated responsive product grid layout

--- fashion-store-react/src/components/FeaturedProducts.jsx
+++ fashion-store-react/src/components/FeaturedProducts.jsx
@@ -35,7 +35,7 @@ function FeaturedProducts() {
       </h2>
 
       {/* 6 COLUMN GRID */}
-      <div className="grid grid-cols-6 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
 
         {products.map((product) => (
 
--- fashion-store-react/src/pages/Products.jsx
+++ fashion-store-react/src/pages/Products.jsx
@@ -84,7 +84,7 @@ function Products() {
       </h1>
 
       {/* PRODUCTS GRID */}
-      <div className="grid grid-cols-6 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
 
         {filteredProducts.map((product) => (
 
